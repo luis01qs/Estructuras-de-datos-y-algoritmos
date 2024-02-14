eede38f60670823887c9ad3d81f559a7c7a6accb
luis01qs <105955279+luis01qs@users.noreply.github.com>
2024-02-14 09:48:26 -0600

    Update My_Library.hpp
    código

--- My_Library.hpp
+++ My_Library.hpp
@@ -1,1 +1,23 @@
+# ifndef My_Library
+# define My_Library
+
+# include <iostream>
+# include <vector>
+
+namespace N
+{
+    // Matemáticas
+    int mcd(int &x, int &y);
+    int factorial(int n);
+
+    // Sorting
+    void sort_insercion(std::vector<int> &a);
+    void sort_seleccion(std::vector<int> &a);
+
+    // Auxiliares
+    void swap(int &a, int &b);
+}
+
+# include "Math.cpp"
+# endif // My_Library
 
