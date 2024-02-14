524c854eebc42d40e22dc51e941362f0ed034f94
luis01qs <luis01qs@hotmail.com>
2024-02-14 10:43:24 -0600

    códigos

---
+++ Otro.cpp
@@ -0,0 +1,12 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> a = {1,2,3,4,5,6,7,8,9,0};
+    for(auto &e:a) cout << e << " ";
+    cout << endl;
+
+    return 0;
+}
